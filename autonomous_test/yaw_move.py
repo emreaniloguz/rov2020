ae1e8f0b9679d0e267c966c88ac11519d2a5b8ae
Enes Bedir <abdullahbedir70@gmail.com>
2019-11-16 07:49:41 +0300

    added new condition in yaw_move

--- autonomous_test/yaw_move.py
+++ autonomous_test/yaw_move.py
@@ -1,24 +1,28 @@
 import time
-# Import mavutil
 from pymavlink import mavutil
 
 master = mavutil.mavlink_connection('udpin:0.0.0.0:15000')
 master.wait_heartbeat()
+
 master.arducopter_arm()
 
 bool = False
 start = time.time()
+cast_param = 0
 
 while True:
     try:
         msg = master.recv_match().to_dict()
+
         if bool == False:
             yaw = msg["yaw"] * 60
             yaw_final = yaw + 30
             bool = True
-        yaw = msg["yaw"]*60
+        if yaw_diff > 180 or yaw_diff < -180:
+            yaw_diff = cast_param
+        yaw = msg["yaw"]*60 - cast_param
         yaw_diff = yaw_final - yaw
-        print ("yaw_diff : ", yaw_diff)
+        print("yaw difference : ", yaw_diff)
         #print("yaw: ", yaw)
 
         if(yaw_diff <2.5 and yaw_diff>-2.5):
@@ -34,6 +38,12 @@ while True:
             yaw_diff = 60
         elif(yaw_diff<=-60):
             yaw_diff = -60
+
+        if yaw_diff > 180 :
+            yaw_diff = - (360 - yaw_diff)
+
+        if yaw_diff < -180 :
+            yaw_diff =  (360 + yaw_diff)
 
         if(time.time()-start>0.2):
             #print(time.time()-start)
