c3717492822661e4210e7ace765d271987d2c009
Baran Marap <b.marap@gmail.com>
2019-12-16 11:16:43 +0300

    yaw sabitleyici kodu eklendi

---
+++ autonomous_test/yawhold.py
@@ -0,0 +1,42 @@
+import time
+from pymavlink import mavutil
+import matplotlib.pyplot as plt
+import numpy as np
+
+master = mavutil.mavlink_connection('udpin:0.0.0.0:15000')
+master.wait_heartbeat()
+
+master.arducopter_arm()
+
+bool = False
+start = time.time()
+
+while True:
+    try:
+        msg = master.recv_match().to_dict()
+
+        if bool == False:
+            yaw = msg["yaw"] * 60
+            yaw_final = yaw + 30
+            bool = True
+        yaw = msg["yaw"]*60
+        yaw_diff = yaw_final - yaw
+        if yaw_diff<0:
+            thrust = 500
+        else :
+            thrust = -500
+        print("yaw difference : ", yaw_diff)
+        if round(thrust)!=0:
+            master.mav.manual_control_send(
+                master.target_system,
+                0,
+                0,
+                500,
+                thrust,
+                0)
+        else:
+            break
+
+    except Exception as msg:
+        # print(msg)      # print exception msg
+        pass
